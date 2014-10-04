bf66e742120c2608e927b98323d25462a42992e1
Joseph <josephcaxtonidowu@yahoo.com>
2014-10-04 16:56:48 +0100

    Fixed IPAddress problems

--- VideoPlayer.m
+++ VideoPlayer.m
@@ -68,6 +68,7 @@
 
 -(void)viewWillAppear:(BOOL)animated{
 	
+    if (!moviePlayerViewController){
     AppDelegate *appDelegate = (AppDelegate *)[UIApplication sharedApplication].delegate;
     
     if(appDelegate.isDeviceConnectedToInternet){
@@ -146,6 +147,7 @@ else
     
     [self.navigationController popViewControllerAnimated:YES];
     
+    }
     }
 }
 
